6acca7f0c240ad929c570b625488d74b9ec51d37
Piero Carneiro <pcarneiro@focusit.pe>
2025-04-01 14:45:20 -0500

    Fix script sql

--- scripts/2025_03_24_add_log_type_inactivity.sql
+++ scripts/2025_03_24_add_log_type_inactivity.sql
@@ -1,1 +1,5 @@
-INSERT INTO [sanna].[log_types] ([id], [state], [date_created], [date_updated], [date_deleted], [icon], [color], [prefix], [name], [suffix]) VALUES (19, 1, '2025-03-21 16:56:55.750', NULL, NULL, N'exit_to_app', '', '', N'Desconectó por inactividad', N'a');
+USE [sanna-topicos-mssql];
+GO
+
+INSERT INTO [sanna].[log_types] ([state], [date_created], [date_updated], [date_deleted], [icon], [color], [prefix], [name], [suffix]) VALUES (1, '2025-03-21 16:56:55.750', NULL, NULL, N'exit_to_app', '', '', N'Desconectó por inactividad', N'a');
+GO
