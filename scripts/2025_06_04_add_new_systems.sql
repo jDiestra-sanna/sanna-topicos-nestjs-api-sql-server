a9165bf5ad1dd649b7f9239cb365d1cee533bc18
Piero Carneiro <pcarneiro@focusit.pe>
2025-06-04 11:30:39 -0500

    SAN-26 / Mejora de registro de consultas

---
+++ scripts/2025_06_04_add_new_systems.sql
@@ -0,0 +1,8 @@
+USE [sanna-topicos-mssql];
+GO
+
+INSERT INTO [sanna].[biological_systems] ([name]) VALUES (N'Sistema Digestivo');
+GO
+
+INSERT INTO [sanna].[biological_systems] ([name]) VALUES (N'Sistema Circulatorio');
+GO
